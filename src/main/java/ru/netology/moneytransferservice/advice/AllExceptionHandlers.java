8ebfd422e3519ea68a4c963d4995eeb4a9cfb9ef
PGK79 <pgk_nev@mail.ru>
2023-02-07 23:11:43 +0500

    Удалены не используемые импорты

--- src/main/java/ru/netology/moneytransferservice/advice/AllExceptionHandlers.java
+++ src/main/java/ru/netology/moneytransferservice/advice/AllExceptionHandlers.java
@@ -4,12 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.netology.authorizationservice.exceptions.InvalidCredentials;
-import ru.netology.authorizationservice.exceptions.UnauthorizedUser;
 import ru.netology.moneytransferservice.exceptions.InputDataException;
 import ru.netology.moneytransferservice.exceptions.TransferException;
-
-import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class AllExceptionHandlers {
