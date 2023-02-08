a7d73148d0997a4cf89d45defcdaee5a74796d86
PGK79 <pgk_nev@mail.ru>
2023-02-09 02:12:25 +0500

    Поставлена аннотация @AllArgsConstructor для уменьшения объема кода

--- src/main/java/ru/netology/moneytransferservice/models/Logger.java
+++ src/main/java/ru/netology/moneytransferservice/models/Logger.java
@@ -1,16 +1,12 @@
 package ru.netology.moneytransferservice.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Logger {
     private String cardFromNumber;
     private String cardToNumber;
     private Amount amount;
-
-    public Logger(String cardFromNumber, String cardToNumber, Amount amount) {
-        this.cardFromNumber = cardFromNumber;
-        this.cardToNumber = cardToNumber;
-        this.amount = amount;
-    }
 }
