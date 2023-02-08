d6df9c882ff474543083b7e39397856d8d9b7647
PGK79 <pgk_nev@mail.ru>
2023-02-08 21:26:46 +0500

    Добавлена аннотация @Data

--- src/main/java/ru/netology/moneytransferservice/models/Confirmer.java
+++ src/main/java/ru/netology/moneytransferservice/models/Confirmer.java
@@ -1,34 +1,9 @@
 package ru.netology.moneytransferservice.models;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Confirmer {
     private final String operationId;
-    private final transient String code;
-
-    public Confirmer(String operationId, String code) {
-        this.operationId = operationId;
-        this.code = code;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Confirmer confirmer = (Confirmer) o;
-        return Objects.equals(operationId, confirmer.operationId) && Objects.equals(code, confirmer.code);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(operationId, code);
-    }
-
-    @Override
-    public String toString() {
-        return "Confirmer{" +
-                "operationId='" + operationId + '\'' +
-                ", code='" + code + '\'' +
-                '}';
-    }
+    private final String code;
 }
