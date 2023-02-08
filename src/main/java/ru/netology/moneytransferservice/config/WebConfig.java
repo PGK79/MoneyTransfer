364ee6da96ba8a10c5c3f3f04740b953a605ff42
PGK79 <pgk_nev@mail.ru>
2023-02-08 21:25:38 +0500

    Добавлено сопоставление с origin

--- src/main/java/ru/netology/moneytransferservice/config/WebConfig.java
+++ src/main/java/ru/netology/moneytransferservice/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("https://serp-ya.github.io");
             }
         };
     }
