af2bd2381c0d558e2f914b57c38692d563f6b6ed
PGK79 <pgk_nev@mail.ru>
2023-02-08 22:18:47 +0500

    Добавление объекта ExceptionDto в ответ

--- src/main/java/ru/netology/moneytransferservice/advice/AllExceptionHandlers.java
+++ src/main/java/ru/netology/moneytransferservice/advice/AllExceptionHandlers.java
@@ -6,16 +6,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.netology.moneytransferservice.exceptions.InputDataException;
 import ru.netology.moneytransferservice.exceptions.TransferException;
+import ru.netology.moneytransferservice.models.ExceptionDto;
 
 @RestControllerAdvice
 public class AllExceptionHandlers {
     @ExceptionHandler(InputDataException.class)
-    public ResponseEntity<String> inputDataExceptionHandler(InputDataException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ExceptionDto> inputDataExceptionHandler(InputDataException e) {
+        return new ResponseEntity<>(new ExceptionDto(e.getMessage(), e.getId()),
+                HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(TransferException.class)
-    public ResponseEntity<String> transferExceptionHandler(TransferException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ExceptionDto> transferExceptionHandler(TransferException e) {
+        return new ResponseEntity<>(new ExceptionDto(e.getMessage(), e.getId()),
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
