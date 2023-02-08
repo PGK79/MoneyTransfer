d6def60903169e5aea13715a560a4b6e676f2fbc
PGK79 <pgk_nev@mail.ru>
2023-02-08 22:15:50 +0500

    Добавлена аннотация @Data

--- src/main/java/ru/netology/moneytransferservice/models/Amount.java
+++ src/main/java/ru/netology/moneytransferservice/models/Amount.java
@@ -1,43 +1,9 @@
 package ru.netology.moneytransferservice.models;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
-import java.util.Objects;
-
+@Data
 public class Amount {
-    private final int value;
+    private final long value;
     private final String currency;
-
-    @JsonCreator
-    public Amount(@JsonProperty("value") int value,
-                  @JsonProperty("currency") String currency) {
-        this.value = value;
-        this.currency = currency;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Amount amount = (Amount) o;
-        return value == amount.value && Objects.equals(currency, amount.currency);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value, currency);
-    }
-
-    @Override
-    public String toString() {
-        return "Amount{" +
-                "value=" + value +
-                ", currency='" + currency + '\'' +
-                '}';
-    }
 }
