9088e5fd02e8748dfedbb3f4e26858d4b1894bea
PGK79 <pgk_nev@mail.ru>
2023-02-09 02:16:52 +0500

    Поставлены аннотации @Data и  @AllArgsConstructor для уменьшения объема кода

--- src/main/java/ru/netology/moneytransferservice/models/Transferer.java
+++ src/main/java/ru/netology/moneytransferservice/models/Transferer.java
@@ -1,60 +1,14 @@
 package ru.netology.moneytransferservice.models;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Transferer {
     private final String cardFromNumber;
     private final String cardFromValidTill;
     private final String cardFromCVV;
     private final String cardToNumber;
     private final Amount amount;
-
-        public Transferer(String cardFromNumber, String cardFromValidTill, String cardFromCVV,
-                      String cardToNumber,Amount amount) {
-        this.cardFromNumber = cardFromNumber;
-        this.cardFromValidTill = cardFromValidTill;
-        this.cardFromCVV = cardFromCVV;
-        this.cardToNumber = cardToNumber;
-        this.amount = amount;
-    }
-
-    public String getCardFromNumber() {
-        return cardFromNumber;
-    }
-
-    public String getCardToNumber() {
-        return cardToNumber;
-    }
-
-    public Amount getAmount() {
-        return amount;
-    }
-
-    public String getCardFromCVV() {
-        return cardFromCVV;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Transferer that = (Transferer) o;
-        return Objects.equals(cardFromNumber, that.cardFromNumber) && Objects.equals(cardFromValidTill, that.cardFromValidTill) && Objects.equals(cardFromCVV, that.cardFromCVV) && Objects.equals(cardToNumber, that.cardToNumber) && Objects.equals(amount, that.amount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cardFromNumber, cardFromValidTill, cardFromCVV, cardToNumber, amount);
-    }
-
-    @Override
-    public String toString() {
-        return "Transferer{" +
-                "cardFromNumber='" + cardFromNumber + '\'' +
-                ", cardFromValidTill='" + cardFromValidTill + '\'' +
-                ", cardFromCVV='" + cardFromCVV + '\'' +
-                ", cardToNumber='" + cardToNumber + '\'' +
-                ", amount=" + amount +
-                '}';
-    }
 }
