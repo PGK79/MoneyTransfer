30a0fdc9d8ad837d4b83cf2870afc380e9d781df
PGK79 <pgk_nev@mail.ru>
2023-02-09 13:46:34 +0500

    Добавлены методы и мапа для работы с информацией по трансакциям

--- src/main/java/ru/netology/moneytransferservice/repositories/TemporaryRepository.java
+++ src/main/java/ru/netology/moneytransferservice/repositories/TemporaryRepository.java
@@ -1,6 +1,7 @@
 package ru.netology.moneytransferservice.repositories;
 
 import org.springframework.stereotype.Repository;
+import ru.netology.moneytransferservice.models.Confirmer;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,6 +12,8 @@ public class TemporaryRepository {
             , "2345234523452345", 10000L, "3456345634563456", 100000L,
             "1111111111111111", 1000000L, "2222222222222222", 100L));
 
+    Map<String, String> transfers = new ConcurrentHashMap<>();
+
     public boolean mapSearch(String cardNumber) {
         return validCards.containsKey(cardNumber);
     }
@@ -20,12 +23,29 @@ public class TemporaryRepository {
     }
 
     public boolean writeOffTheCard(String cardFromNumber, Long transferAmount) {
-        validCards.put(cardFromNumber, validCards.get(cardFromNumber) - transferAmount);
+        validCards.replace(cardFromNumber, validCards.get(cardFromNumber) - transferAmount);
         return true;
     }
 
     public boolean putMoneyTheCard(String cardToNumber, Long transferAmount) {
-        validCards.put(cardToNumber, validCards.get(cardToNumber) + transferAmount);
+        validCards.replace(cardToNumber, validCards.get(cardToNumber) + transferAmount);
         return true;
+    }
+
+    public boolean saveTransfers(String operationId, String code) {
+        transfers.putIfAbsent(operationId, code);
+        return true;
+    }
+
+    public boolean getTransferId(Confirmer confirmer) {
+        return transfers.containsKey(confirmer.getOperationId());
+    }
+
+    public boolean getVerificationCode(Confirmer confirmer) {
+        return transfers.get(confirmer.getOperationId()).equals(confirmer.getCode());
+    }
+
+    public boolean checkForTransactionRecord(){
+        return transfers.isEmpty();
     }
 }
