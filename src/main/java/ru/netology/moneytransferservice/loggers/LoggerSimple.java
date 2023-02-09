672b9a74a8ec4098d225dfa8a1eb6f6244754e15
PGK79 <pgk_nev@mail.ru>
2023-02-09 14:59:36 +0500

    Добавлен метод логирования

--- src/main/java/ru/netology/moneytransferservice/loggers/LoggerSimple.java
+++ src/main/java/ru/netology/moneytransferservice/loggers/LoggerSimple.java
@@ -4,10 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.netology.moneytransferservice.models.Amount;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 public class LoggerSimple {
     private String cardFromNumber;
     private String cardToNumber;
     private Amount amount;
+
+    public synchronized void logFile(LoggerSimple loggerSimple, String result) {
+        SimpleDateFormat dateNow = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
+        String data = dateNow.format(new Date());
+        String log = "[" + data + "] " + "Карта списания: " + loggerSimple.getCardFromNumber()
+                + " Карта зачисления: " + loggerSimple.getCardToNumber() + " " + " Cумма: "
+                + loggerSimple.getAmount().getValue() + " " + loggerSimple.getAmount().getCurrency()
+                + " Комиссия: " + loggerSimple.getAmount().getValue() / 100 + " "
+                + loggerSimple.getAmount().getCurrency() + " Результат операции: " + result + "\n";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("logs/logFile.log",
+                true))) {
+            bw.write(log);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
