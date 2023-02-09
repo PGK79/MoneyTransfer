1594a16a22dcc9854efaa4aedf2e02222eb404b4
PGK79 <pgk_nev@mail.ru>
2023-02-10 00:31:27 +0500

    Логирование исключения передано классу исключения

--- src/main/java/ru/netology/moneytransferservice/exceptions/InputDataException.java
+++ src/main/java/ru/netology/moneytransferservice/exceptions/InputDataException.java
@@ -1,12 +1,19 @@
 package ru.netology.moneytransferservice.exceptions;
 
 import lombok.Data;
+import ru.netology.moneytransferservice.loggers.LoggerSimple;
 
 @Data
 public class InputDataException extends RuntimeException {
     private final int id;
+
     public InputDataException(String msg, int id) {
         super(msg);
         this.id = id;
+    }
+    public InputDataException(String msg, int id, LoggerSimple logger) {
+        super(msg);
+        this.id = id;
+        logger.logFile(msg);
     }
 }
