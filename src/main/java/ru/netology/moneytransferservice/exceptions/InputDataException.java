8c4dbe51a7d27f964ec0aa71968fe5f7fa781317
PGK79 <pgk_nev@mail.ru>
2023-02-10 17:43:45 +0500

    Reformat code

--- src/main/java/ru/netology/moneytransferservice/exceptions/InputDataException.java
+++ src/main/java/ru/netology/moneytransferservice/exceptions/InputDataException.java
@@ -3,6 +3,8 @@ package ru.netology.moneytransferservice.exceptions;
 import lombok.Data;
 import ru.netology.moneytransferservice.loggers.LoggerSimple;
 
+import java.util.Objects;
+
 @Data
 public class InputDataException extends RuntimeException {
     private final int id;
@@ -12,5 +14,18 @@ public class InputDataException extends RuntimeException {
         super(msg);
         this.id = id;
         this.loggerSimple = logger;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InputDataException that = (InputDataException) o;
+        return id == that.id && Objects.equals(loggerSimple, that.loggerSimple);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, loggerSimple);
     }
 }
