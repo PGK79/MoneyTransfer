92af95c748c58a2c9d8d626acb937d6df815cdf0
PGK79 <pgk_nev@mail.ru>
2023-02-10 17:39:48 +0500

    Добавлен loggerSimple

--- src/main/java/ru/netology/moneytransferservice/exceptions/ConfirmationException.java
+++ src/main/java/ru/netology/moneytransferservice/exceptions/ConfirmationException.java
@@ -2,14 +2,17 @@ package ru.netology.moneytransferservice.exceptions;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import ru.netology.moneytransferservice.loggers.LoggerSimple;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class ConfirmationException extends RuntimeException {
     private final int id;
+    private final LoggerSimple loggerSimple;
 
-    public ConfirmationException(String msg, int id) {
+    public ConfirmationException(String msg, int id, LoggerSimple loggerSimple) {
         super(msg);
         this.id = id;
+        this.loggerSimple = loggerSimple;
     }
 }
