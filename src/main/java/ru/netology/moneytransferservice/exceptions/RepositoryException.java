2e2d7f82041b654ff092c8aa0fdd6b90494eaece
PGK79 <pgk_nev@mail.ru>
2023-02-10 17:40:24 +0500

    Добавлен loggerSimple

--- src/main/java/ru/netology/moneytransferservice/exceptions/RepositoryException.java
+++ src/main/java/ru/netology/moneytransferservice/exceptions/RepositoryException.java
@@ -2,14 +2,17 @@ package ru.netology.moneytransferservice.exceptions;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import ru.netology.moneytransferservice.loggers.LoggerSimple;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class RepositoryException extends RuntimeException {
     private final int id;
+    private final LoggerSimple loggerSimple;
 
-    public RepositoryException(String msg, int id) {
+    public RepositoryException(String msg, int id, LoggerSimple loggerSimple) {
         super(msg);
         this.id = id;
+        this.loggerSimple = loggerSimple;
     }
 }
