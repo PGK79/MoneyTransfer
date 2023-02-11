767c88ed2c204b3d4a4b025d6493d500b4b8a9dd
PGK79 <pgk_nev@mail.ru>
2023-02-11 22:08:00 +0500

    Добавлена еще одна точка на localhost 3000

--- src/main/java/ru/netology/moneytransferservice/config/WebConfig.java
+++ src/main/java/ru/netology/moneytransferservice/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://serp-ya.github.io");
+                        .allowedOrigins("https://serp-ya.github.io")
+                        .allowedOrigins("http://localhost:3000");
             }
         };
     }
